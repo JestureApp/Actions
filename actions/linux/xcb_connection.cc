16c38bda519fc2b418e4357bfa757e976e5b4969
Carson Storm <carson.storm@outlook.com>
2023-01-30 11:45:24 -0700

    Handle errors in connection

--- actions/linux/xcb_connection.cc
+++ actions/linux/xcb_connection.cc
@@ -5,10 +5,12 @@
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "actions/linux/xcb_error.h"
 #include "actions/linux/xcb_promise.h"
 #include "actions/promise.h"
 
 namespace actions {
+
 XcbConnection::XcbConnection(xcb_connection_t* conn, xcb_screen_t* screen,
                              xcb_key_symbols_t* key_symbols)
     : conn(conn), screen(screen), key_symbols(key_symbols) {}
@@ -39,15 +41,23 @@ XcbConnection& XcbConnection::operator=(XcbConnection&& other) noexcept {
     return *this;
 }
 
-absl::StatusOr<std::unique_ptr<XcbConnection>> XcbConnection::Open() {
+// TODO: clean up
+absl::StatusOr<std::unique_ptr<XcbConnection>> XcbConnection::Open() noexcept {
+    absl::Status status = absl::OkStatus();
     int screen_num;
     xcb_connection_t* conn = xcb_connect(nullptr, &screen_num);
 
     if (conn == nullptr)
         return absl::UnavailableError("Could not connected to X server");
 
-    // TODO: check for error
+    status.Update(XcbError(xcb_connection_has_error(conn)));
 
+    if (!status.ok()) {
+        xcb_disconnect(conn);
+        return status;
+    }
+
+    // Get the primary screen
     const xcb_setup_t* setup = xcb_get_setup(conn);
     xcb_screen_iterator_t screen_iter = xcb_setup_roots_iterator(setup);
 
@@ -60,16 +70,23 @@ absl::StatusOr<std::unique_ptr<XcbConnection>> XcbConnection::Open() {
 
     xcb_screen_t* screen = screen_iter.data;
 
+    // Ensure that XTest extension is present
     const xcb_query_extension_reply_t* xtest_data =
         xcb_get_extension_data(conn, &xcb_test_id);
 
-    // TODO: Check for error
+    status.Update(XcbError(xcb_connection_has_error(conn)));
+
+    if (!status.ok()) {
+        xcb_disconnect(conn);
+        return status;
+    }
 
     if (!xtest_data->present) {
         xcb_disconnect(conn);
         return absl::UnavailableError("XTest extension not present");
     }
 
+    // Setup key symbol translation
     xcb_key_symbols_t* key_symbols = xcb_key_symbols_alloc(conn);
 
     return std::unique_ptr<XcbConnection>(
