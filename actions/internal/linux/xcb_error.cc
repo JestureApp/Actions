316b3eb05d69dd9ffd9a261bc857a076ac83a5ab
Carson Storm <carson.storm@outlook.com>
2023-02-01 17:13:14 -0700

    add xcb_error

---
+++ actions/internal/linux/xcb_error.cc
@@ -0,0 +1,58 @@
+#include "actions/internal/linux/xcb_error.h"
+
+#include <absl/status/status.h>
+#include <xcb/xcb.h>
+
+namespace actions::internal::linux {
+absl::Status XcbStatus(int code) noexcept {
+    switch (code) {
+        case 0:
+            return absl::OkStatus();
+
+        case XCB_CONN_ERROR:
+            return absl::InternalError(
+                "xcb connection errors because of socket, pipe "
+                "and other stream errors. (code: XCB_CONN_ERROR)");
+
+        case XCB_CONN_CLOSED_EXT_NOTSUPPORTED:
+            return absl::InternalError(
+                "xcb connection shutdown because of extension not supported. "
+                "(code: XCB_CONN_CLOSED_EXT_NOTSUPPORTED)");
+
+        case XCB_CONN_CLOSED_MEM_INSUFFICIENT:
+            return absl::ResourceExhaustedError(
+                "Insufficient memory. (code: "
+                "XCB_CONN_CLOSED_MEM_INSUFFICIENT)");
+
+        case XCB_CONN_CLOSED_REQ_LEN_EXCEED:
+            return absl::InternalError(
+                "Connection closed, exceeding request length that server "
+                "accepts. (code: XCB_CONN_CLOSED_REQ_LEN_EXCEED)");
+
+        case XCB_CONN_CLOSED_PARSE_ERR:
+            return absl::InternalError(
+                "Connection closed, error during parsing display string. "
+                "(code: XCB_CONN_CLOSED_PARSE_ERR)");
+
+        case XCB_CONN_CLOSED_INVALID_SCREEN:
+            return absl::InternalError(
+                "Connection closed because the server does not have a screen "
+                "matching the display. (code: XCB_CONN_CLOSED_INVALID_SCREEN)");
+
+        case XCB_CONN_CLOSED_FDPASSING_FAILED:
+            return absl::InternalError(
+                "Connection closed because some FD passing operation failed. "
+                "(code: XCB_CONN_CLOSED_FDPASSING_FAILED)");
+
+        default:
+            return absl::UnknownError(
+                "Unrecognized Xcb error. (code: " + std::to_string(code) + ")");
+    }
+}
+
+absl::Status XcbErrorToStatus(xcb_connection_t *conn,
+                              xcb_generic_error_t *error) noexcept {
+    // TODO
+    return absl::UnimplementedError("TODO");
+}
+}  // namespace actions::internal::linux
